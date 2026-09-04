a1f7475cf086f8dc3d43b4b448b7df9d8d8d72ba
AshwathNagarajan <ashwathnagarajan14@gmail.com>
2026-09-04 11:59:42 +0530

    ENV changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  envPrefix: ["VITE_", "API_"],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
